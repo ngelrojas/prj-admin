1d2ab454e1047f86830595af1f8284a45894b1e3
ngel rojas <aomarrojasp@gmail.com>
2023-10-04 20:54:04 -0300

    first commit

---
+++ node-admin/src/contoller/auth.controller.ts
@@ -0,0 +1,11 @@
+import {Request, Response} from "express";
+import {RegisterValidation} from "../validation/register.validation";
+
+export const Register = (req:Request, res: Response) => {
+    const body = req.body;
+    const {error} = RegisterValidation.validate(body);
+    if(error){
+        return res.status(400).send(error.details);
+    }
+    res.send(req.body);
+}
---
+++ node-admin/src/index.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response} from 'express';
+import cors from 'cors';
+import {routes} from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
+
+routes(app);
+app.listen(8000, () => {
+    console.log("listening on port 8000")
+});
