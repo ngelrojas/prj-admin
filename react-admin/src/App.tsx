e41f3bf318690f1923771613f0d89ef76f131c43
ngel rojas <aomarrojasp@gmail.com>
2023-10-15 14:49:56 -0300

    add: adding router for pages dashboard and users

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -2,47 +2,30 @@ import React from 'react';
 import './App.css';
 import Nav from "./components/Nav";
 import Menu from "./components/Menu";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
 
-      <div className="container-fluid">
-        <div className="row">
-            <Menu />
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-            <div className="table-responsive small">
-              <table className="table table-striped table-sm">
-                <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
-                </tr>
-                </tbody>
-              </table>
+    return (
+        <BrowserRouter basename="/">
+            <div className="App">
+              <Nav />
+              <div className="container-fluid">
+                <div className="row">
+                    <Menu />
+                  <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                      <Routes>
+                          <Route path={"/"} element={<Dashboard />} />
+                          <Route path={"/users"} element={<Users />} />
+                      </Routes>
+                  </main>
+                </div>
+              </div>
             </div>
-          </main>
-        </div>
-      </div>
-      <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous"></script>
-
-      <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossOrigin="anonymous"></script><script src="dashboard.js"></script>
-    </div>
+        </BrowserRouter>
   );
 }
 
--- react-admin/src/components/Menu.tsx
+++ react-admin/src/components/Menu.tsx
@@ -1,29 +1,27 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
-export default function Menu() {
+const Menu = () => {
     return(
         <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
             <div className="offcanvas-md offcanvas-end bg-body-tertiary" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
-                <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
-                </div>
+
                 <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
                     <ul className="nav flex-column">
                         <li className="nav-item">
-                            <a className="nav-link d-flex align-items-center gap-2 active">
+                            <Link to={"/"} className="nav-link d-flex align-items-center gap-2 active">
                                 Dashboard
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link d-flex align-items-center gap-2">
-                                Orders
-                            </a>
+                            <Link to={"/users"} className="nav-link d-flex align-items-center gap-2" >
+                                Users
+                            </Link>
                         </li>
                     </ul>
-
                 </div>
             </div>
         </div>
     )
 }
+export default Menu;
