786623c0116b8f616107ec61ce7f95644fe209ae
ngel rojas <aomarrojasp@gmail.com>
2023-10-20 13:59:54 -0300

    add: adding react-redux and setup for redux

--- react-admin/src/index.tsx
+++ react-admin/src/index.tsx
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import axios from "axios";
+import { Provider } from 'react-redux';
+import {configureStore} from "./redux/configureStore";
 
 axios.defaults.baseURL = 'http://localhost:8000/api';
 axios.defaults.withCredentials = true;
+const store = configureStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
+      <Provider store={store}>
         <App />
+      </Provider>
   </React.StrictMode>
 );
 
